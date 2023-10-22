864f93f72efe5ca4a2af412a981b2c0c25a57438
NikhilaVarikuti <144110595+NikhilaVarikuti@users.noreply.github.com>
2023-10-22 21:03:15 +0530

    Add files via upload

---
+++ src/components/Contact.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <section>
+      <h2>Contact</h2>
+      <p>
+        I'm constantly open to new chances and collaborations and eager to learn
+        new things.
+      </p>
+      <ul>
+        <li>
+          <strong>Email:</strong>{" "}
+          <a href="mailto:nikhilavarikuti@gmail.com">Email</a>
+        </li>
+        <li>
+          <strong>GitHub:</strong>{" "}
+          <a
+            href="https://github.com/nikhilavarikuti "
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            github
+          </a>
+        </li>
+      </ul>
+      <p>Connecting with you is something I'm excited about!</p>
+    </section>
+  );
+};
+
+export default Contact;
---
+++ src/components/Projects.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Projects = () => {
+  return (
+    <section id="projects">
+      <h2>Projects</h2>
+      <h5>MERN</h5>
+
+      <a href="https://nikhilavarikuti.github.io/MERN_REACT_TASK_5/">
+        GitHub Repository
+      </a>
+      {/* Add your project components or details here */}
+    </section>
+  );
+};
+
+export default Projects;
